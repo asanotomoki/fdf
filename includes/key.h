d252a211b326ef18467f618d8d15163f327141bc
浅野智貴 <mirai20190820@gmail.com>
2022-10-31 19:34:05 +0900

    minilibx linux

--- includes/key.h
+++ includes/key.h
@@ -6,7 +6,7 @@
 /*   By: asanotomoki <asanotomoki@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 16:47:19 by asanotomoki       #+#    #+#             */
-/*   Updated: 2022/10/31 15:41:10 by asanotomoki      ###   ########.fr       */
+/*   Updated: 2022/10/31 16:52:26 by asanotomoki      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 # define KEY_H
 
 # ifdef __APPLE__
-#  define KEY_PLUS	41
-#  define KEY_MINUS 27
-#  define KEY_LEFT 123
-#  define KEY_RIGHT 124
-#  define KEY_DOWN 125
-#  define KEY_UP 	126
-#  define KEY_ESC 53
-#  define KEY_X 7
-#  define KEY_I	34
-#  define KEY_P	35
-#  define KEY_LESS 43
-#  define KEY_MORE 47
-#  define MOUSE_UP 4
-#  define MOUSE_DOWN 5
+#  define KEY_PLUS		41
+#  define KEY_MINUS 	27
+#  define KEY_LEFT		123
+#  define KEY_RIGHT		124
+#  define KEY_DOWN		125
+#  define KEY_UP		126
+#  define KEY_ESC		53
+#  define KEY_X			7
+#  define KEY_I			34
+#  define KEY_P			35
+#  define KEY_LESS		43
+#  define KEY_MORE		47
+#  define MOUSE_UP		4
+#  define MOUSE_DOWN	5
 # endif
 
 # ifdef __linux__
-#  define MOUSE_UP	4
+#  define MOUSE_UP		4
 #  define MOUSE_DOWN	5
 #  define KEY_LEFT		65361
 #  define KEY_RIGHT		65363
